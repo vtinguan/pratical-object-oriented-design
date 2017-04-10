68521ddefef82d3a6e6e5a0756865234a86500bc
vtinguan <vinicius.tinguan@gmail.com>
2017-04-10 00:51:05 -0300

    COMMIT 2 - ADD GEAR INCHES BEHAVIOUR (BROKE THE CONSTRUCTOR)

--- src/ruby/gear.rb
+++ src/ruby/gear.rb
@@ -1,13 +1,19 @@
 class Gear
   attr_reader :chainring, :cog
 
-  def initialize(chainring, cog)
+  def initialize(chainring, cog, rim)
     @chainring = chainring
     @cog = cog
+    @rim = rim
+    @tire = tire
   end
 
   def ratio
     chainring / cog.to_f
+  end
+
+  def gear_inches
+    ratio * (rim + (tire * 2))
   end
 end
 
